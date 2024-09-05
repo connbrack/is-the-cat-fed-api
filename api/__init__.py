fbdad92bba8399887c9d115a285d484e28d6f95a
Connor Brackley <noreply@mail.com>
2024-09-04 20:15:41 -0400

    Switched to external database

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,17 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from dotenv import load_dotenv
+load_dotenv()
 
 db=SQLAlchemy()
+dbURL = str(os.getenv('dbURL'))
 
 def create_app():
   app = Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///fed.db'
+  app.config['SQLALCHEMY_DATABASE_URI'] = dbURL
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   db.init_app(app)
   CORS(app)
